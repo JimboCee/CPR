d70bcd0fdc721e78d1898593a079d653071cdd92
JimboCee <49816802+JimboCee@users.noreply.github.com>
2022-12-17 14:35:34 +0000

    Initial pages and styling b4 design eval.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>CPR Application</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SPA - Learn and perform CPR
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+    <div className="options"></div>
+    
+    <div className="learnCPR">
+      <button type="button">Learn CPR</button>
     </div>
+    {/* ROUTE TO LEARN CPR - STEP BY STEP INSTRUCTIONS */}
+
+    <div className="performCPR">
+      <button type="button">Perform CPR</button>
+    </div>
+    {/* ROUTE TO PERFORM CPR - STEP BY STEP & FEATURES (METRONOME) */}
+
+    <div className="aboutCPR">
+      <button type="button">About</button>
+    </div>
+    {/* ROUTE TO ABOUT CPR - STEP BY STEP & FEATURES (METRONOME) */}
+    
+  </div>
   );
 }
 
